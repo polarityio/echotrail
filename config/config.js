db09164052f1cf8a10efb65be2b306987823b1fb
Ed <ed@polarity.io>
2023-12-26 15:56:06 -0500

    Initial commit

---
+++ config/config.js
@@ -0,0 +1,126 @@
+module.exports = {
+  /**
+   * Name of the integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @required
+   */
+  name: 'EchoTrail',
+  /**
+   * The acronym that appears in the notification window when information from this integration
+   * is displayed.  Note that the acronym is included as part of each "tag" in the summary information
+   * for the integration.  As a result, it is best to keep it to 4 or less characters.  The casing used
+   * here will be carried forward into the notification window.
+   *
+   * @type String
+   * @required
+   */
+  acronym: 'ET',
+  defaultColor: 'light-gray',
+  onDemandOnly: true,
+  /**
+   * Description for this integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @optional
+   */
+  description:
+    'Access data points detailing what processes are, how they typically behave, who wrote them, and Security Intel on how they are used by threat actors.',
+  entityTypes: ['SHA256'],
+  customTypes: [
+    {
+      key: 'windowsExecutableFile',
+      regex: /[\w-]{2,256}\.(?:exe)/
+    }
+  ],
+  /**
+   * An array of style files (css or less) that will be included for your integration. Any styles specified in
+   * the below files can be used in your custom template.
+   *
+   * @type Array
+   * @optional
+   */
+  styles: ['./styles/styles.less'],
+  /**
+   * Provide custom component logic and template for rendering the integration details block.  If you do not
+   * provide a custom template and/or component then the integration will display data as a table of key value
+   * pairs.
+   *
+   * @type Object
+   * @optional
+   */
+  block: {
+    component: {
+      file: './components/block.js'
+    },
+    template: {
+      file: './templates/block.hbs'
+    }
+  },
+  summary: {
+    component: {
+      file: './components/summary.js'
+    },
+    template: {
+      file: './templates/summary.hbs'
+    }
+  },
+  request: {
+    // Provide the path to your certFile. Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    cert: '',
+    // Provide the path to your private key. Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    key: '',
+    // Provide the key passphrase if required.  Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    passphrase: '',
+    // Provide the Certificate Authority. Leave an empty string to ignore this option.
+    // Relative paths are relative to the integration's root directory
+    ca: '',
+    // An HTTP proxy to be used. Supports proxy Auth with Basic Auth, identical to support for
+    // the url parameter (by embedding the auth info in the uri)
+    proxy: ''
+  },
+  logging: {
+    level: 'info' //trace, debug, info, warn, error, fatal
+  },
+  /**
+   * Options that are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
+   * as an array of option objects.
+   *
+   * @type Array
+   * @optional
+   */
+  options: [
+    {
+      key: 'apiKey',
+      name: 'EchoTrail API Key',
+      description: 'API Key for the EchoTrail API',
+      default: '',
+      type: 'password',
+      userCanEdit: false,
+      adminOnly: true
+    },
+    {
+      key: 'suspiciousThreshold',
+      name: 'Anomalous Process Threshold for SHA256',
+      description:
+        'SHA256 values with an EchoTrail prevalence score below or equal to this value will be flagged as anomalous. Defaults to 20. Set to -1 to disable. This option should be visible to all users. This option is only applicable to SHA256 lookups.',
+      default: 20,
+      type: 'number',
+      userCanEdit: false,
+      adminOnly: false
+    },
+    {
+      key: 'commonThreshold',
+      name: 'Common Process Threshold for SHA256',
+      description:
+        'SHA256 values with an EchoTrail prevalence score above or equal to this value will be flagged as a common process. Defaults to 80.  Set to -1 to disable. This option should be visible to all users. This option is only applicable to SHA256 lookups.',
+      default: 80,
+      type: 'number',
+      userCanEdit: false,
+      adminOnly: false
+    }
+  ]
+};
---
+++ integration.js
@@ -0,0 +1,191 @@
+'use strict';
+
+const request = require('postman-request');
+const config = require('./config/config');
+const async = require('async');
+const fs = require('fs');
+
+let Logger;
+let requestWithDefaults;
+
+const MAX_PARALLEL_LOOKUPS = 10;
+
+function startup(logger) {
+  Logger = logger;
+
+  let requestOptions = {};
+
+  if (typeof config.request.cert === 'string' && config.request.cert.length > 0) {
+    requestOptions.cert = fs.readFileSync(config.request.cert);
+  }
+
+  if (typeof config.request.key === 'string' && config.request.key.length > 0) {
+    requestOptions.key = fs.readFileSync(config.request.key);
+  }
+
+  if (typeof config.request.passphrase === 'string' && config.request.passphrase.length > 0) {
+    requestOptions.passphrase = config.request.passphrase;
+  }
+
+  if (typeof config.request.ca === 'string' && config.request.ca.length > 0) {
+    requestOptions.ca = fs.readFileSync(config.request.ca);
+  }
+
+  if (typeof config.request.proxy === 'string' && config.request.proxy.length > 0) {
+    requestOptions.proxy = config.request.proxy;
+  }
+
+  if (typeof config.request.rejectUnauthorized === 'boolean') {
+    requestOptions.rejectUnauthorized = config.request.rejectUnauthorized;
+  }
+
+  requestWithDefaults = request.defaults(requestOptions);
+}
+
+/**
+ *
+ * @param entities
+ * @param options
+ * @param cb
+ */
+function doLookup(entities, options, cb) {
+  const lookupResults = [];
+  const tasks = [];
+
+  Logger.trace({ entities }, 'doLookup');
+
+  entities.forEach((entity) => {
+    const requestOptions = {
+      uri: `https://api.echotrail.io/insights/${entity.value}`,
+      method: 'GET',
+      headers: {
+        'x-api-key': options.apiKey
+      },
+      json: true
+    };
+
+    tasks.push(function (done) {
+      Logger.debug({ requestOptions: requestOptions }, 'Sending Request');
+      requestWithDefaults(requestOptions, function (error, res, body) {
+        if (error) {
+          Logger.error(error, 'HTTP Request Error');
+          return done({
+            detail: 'HTTP Request Error',
+            error
+          });
+        }
+
+        Logger.trace({ body: body, status: res ? res.statusCode : 'N/A' }, 'Received Response');
+
+        if (res.statusCode === 200) {
+          done(null, {
+            entity: entity,
+            body: body
+          });
+        } else if (res.statusCode === 403) {
+          done({
+            detail: `Invalid API Key`,
+            body
+          });
+        } else if (res.statusCode === 403) {
+          done({
+            detail: `API Usage Limit Reached - Too many requests`,
+            body
+          });
+        } else {
+          done({
+            detail: `Unexpected HTTP Status Code Received: ${res.statusCode}`,
+            body
+          });
+        }
+      });
+    });
+  });
+
+  async.parallelLimit(tasks, MAX_PARALLEL_LOOKUPS, (err, results) => {
+    if (err) {
+      cb({
+        detail: err.error,
+        ...err
+      });
+      return;
+    }
+
+    results.forEach((result) => {
+      if (result.body) {
+        lookupResults.push({
+          entity: result.entity,
+          data: {
+            summary: getSummaryTags(result.body, result.entity, options),
+            details: result.body
+          }
+        });
+      } else {
+        lookupResults.push({
+          entity: result.entity,
+          data: null
+        });
+      }
+    });
+
+    Logger.debug({ lookupResults }, 'Results');
+
+    cb(null, lookupResults);
+  });
+}
+
+function getSummaryTags(body, entity, options) {
+  const tags = [];
+
+  // If there is a message then we didn't get a result back
+  // The message typically says the executable has not been observed
+  // Note that the API still returns a 200
+  if (body.message) {
+    tags.push(`Not observed`);
+  } else {
+    // string to number conversion required for v4 servers which may not send a numberic value
+    if (+options.commonThreshold != -1 && body.eps >= options.commonThreshold && entity.isHash) {
+      tags.push({
+        text: `EPS: ${body.eps}`,
+        icon: 'check',
+        classes: ['stripe-bg-green']
+      });
+    } else if (
+      +options.suspiciousThreshold != -1 &&
+      body.eps <= options.suspiciousThreshold &&
+      entity.isHash
+    ) {
+      tags.push({
+        text: `EPS: ${body.eps}`,
+        icon: 'exclamation',
+        classes: ['stripe-bg-orange']
+      });
+    } else {
+      tags.push(`EPS: ${body.eps}`);
+    }
+  }
+
+  return tags;
+}
+
+function validateOptions(userOptions, cb) {
+  let errors = [];
+
+  if (
+    typeof userOptions.apiKey.value !== 'string' ||
+    (typeof userOptions.apiKey.value === 'string' && userOptions.apiKey.value.length === 0)
+  ) {
+    errors.push({
+      key: 'apiKey',
+      message: 'You must provide an Echotrail API key'
+    });
+  }
+
+  cb(null, errors);
+}
+
+module.exports = {
+  doLookup,
+  startup,
+  validateOptions
+};
